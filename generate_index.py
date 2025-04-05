cd202f3d4eea2c3fbf0f04545dca61e6714556c2
Hoshinonono <96542825+Hoshinonono@users.noreply.github.com>
2025-04-05 15:19:54 +0900

    Update generate_index.py

--- generate_index.py
+++ generate_index.py
@@ -1,25 +1,47 @@
+# index.mdの内容を生成するpythonファイルです
+# フォルダ探索をすることで目次を自動生成します
 import os
 
-MARKDOWN_DIR = "./"
+# 現在のディレクトリファイル
 INDEX_FILE = "index.md"
+depth = int(2)
 
-def generate_index():
-    """docs フォルダ内の Markdown ファイルを一覧表示する index.md を生成"""
-    files = [f for f in os.listdir(MARKDOWN_DIR) if f.endswith(".md") and f != "index.md" and f != "README.md"]
-    files.sort()  # アルファベット順にソート
+# フォルダ名かファイル名が以下に一致するものは探索を禁止する
+ban_list = ['.github', 'README.md']
 
-    index_content = "# Documentation Index\n\n"
-    
-    index_content += "[![GitHub Pepository](https://img.shields.io/static/v1?label=GitHub+Pepository&message=+&color=FC02FF&logo=github)](https://github.com/Hoshinonono/study)\n\n";
-    index_content += "目次は自動生成です。\n\n"
-    index_content += "## Pages\n\n"
+# index.mdに書き込む内容をcontentに追加する
+content = "# Documentation Index\n\n"
+content += "[![GitHub Pepository](https://img.shields.io/static/v1?label=GitHub+Pepository&message=+&color=FC02FF&logo=github)](https://github.com/Hoshinonono/study)\n\n";
+content += "目次は自動生成です。\n\n"
+content += "## Pages\n\n"
+
+def generate_index(path):
+    global content
+    global depth
+    """path フォルダ内のディレクトリまたはファイルを一覧表示する"""
+    files = [f for f in os.listdir(path)]
 
     for file in files:
-        file_title = file.replace(".md", "").replace("-", " ").title()
-        index_content += f"- [{file_title}]({file})\n"
+        if os.path.isfile(os.path.join(path, file)) and (not file in ban_list) and file.endswith(".md"):
+            file_title = file.replace(".md", "").replace("-", " ").title()
+            file_path = path + "/" + file
+            content += f"- [{file_title}]({file_path})\n"
 
-    with open(INDEX_FILE, "w", encoding="utf-8") as f:
-        f.write(index_content)
+    # 深さが6よりも大きくなる場合は探索を打ち切る
+    # Markdownの ### h3 みたいなのが6までだったと思うので
+    if depth >= 6: return
+
+    for folder in files:
+        if os.path.isdir(os.path.join(path, folder)) and (not folder in ban_list):
+            depth += 1
+            content += "#" * depth
+            content += f" {folder}\n\n"
+            generate_index(path + "/" + folder)
+            depth -= 1
+    
+    content += "\n"
 
 if __name__ == "__main__":
-    generate_index()
+    generate_index("./")
+    with open(INDEX_FILE, "w", encoding="utf-8") as f:
+        f.write(content)
